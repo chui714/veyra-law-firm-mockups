503ebfbe7f3b6cf0abc3e0fff77797ac0178afde
Cesar Rojas <cesarrojas@MacBookPro.lan>
2026-04-26 12:59:06 -0700

    feat: add Veyra Digital label to modern mockup

--- src/components/modern/ModernHome.tsx
+++ src/components/modern/ModernHome.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ModernHero from "./ModernHero";
 import ModernServices from "./ModernServices";
 import ModernApproach from "./ModernApproach";
@@ -14,6 +15,14 @@ export default function ModernHome() {
       <ModernAttorney />
       <ModernStats />
       <ModernContactFooter />
+
+      <Link
+        href="/"
+        className="fixed bottom-5 left-5 z-50 hidden border border-[#d8d8d6] bg-[#f8f7f4]/90 px-4 py-3 text-[10px] font-semibold uppercase tracking-[0.22em] text-[#111111] shadow-sm backdrop-blur transition hover:border-[#111111] md:inline-flex"
+      >
+        Veyra Digital Concept
+      </Link>
+
     </main>
   );
 }
